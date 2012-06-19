0b70de550ec383e9c9d0a0b4956b547416be414e
xuanhuangyiqi <xuanhuangyiqi@126.com>
2012-06-19 16:21:01 +0800

    update change_sql

--- scripts/sql/change_06190325.sql
+++ scripts/sql/change_06190325.sql
@@ -1,3 +1,3 @@
-alter table member language_id language varchar(100);
-alter table member location_id location varchar(100);
-alter table member job_id job varchar(100);
+alter table member change language_id language varchar(100);
+alter table member change location_id location varchar(100);
+alter table member change job_id job varchar(100);
