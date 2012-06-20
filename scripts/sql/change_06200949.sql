825da0800c836cf677804432d1d1edd36e90a4e8
xuanhuangyiqi <xuanhuangyiqi@126.com>
2012-06-20 10:14:16 +0800

    add column in member

---
+++ scripts/sql/change_06200949.sql
@@ -0,0 +1,11 @@
+ALTER table member ADD target varchar(100);
+ALTER table member ADD street varchar(100);
+ALTER table member ADD city varchar(100);
+ALTER table member ADD state varchar(100);
+ALTER table member ADD zip varchar(100);
+ALTER table member ADD country varchar(100);
+ALTER table member ADD mobile varchar(100);
+ALTER table member ADD mobile_country_code varchar(100);
+ALTER table member ADD mail varchar(100);
+ALTER table member ADD mail_option int(1);
+
