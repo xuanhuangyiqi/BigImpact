daec48ee551341f5fc47f3300e4713e653eee597
xuanhuangyiqi <xuanhuangyiqi@126.com>
2012-06-25 11:08:55 +0800

    stage2 sql

---
+++ scripts/sql/create_0620.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS `event`(
+    `id` int(11) unsigined NOT NULL AUTO_INCREMENT,
+    `url_token` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    `member_id` int(11) NOT NULL,
+    `title` int(11) NOT NULL,
+    `description` varchar(1024),
+    `start_date` varchar(50),
+    `end_date` varchar(50),
+    `etype` int(1) COMMENT '0:for 1 day, 1:duration',
+    `fields` varchar(1024),
+    `locations` varchar(1024),
+    `target` varchar(1024),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `url_token` (`url_token`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `offer`(
+    `id` int(11) unsigined NOT NULL AUTO_INCREMENT,
+    `url_token` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    `member_id` int(11) NOT NULL,
+    `title` int(11) NOT NULL,
+    `description` varchar(1024),
+    `fields` varchar(1024),
+    `locations` varchar(1024),
+    `target` varchar(1024),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `url_token` (`url_token`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `need`(
+    `id` int(11) unsigined NOT NULL AUTO_INCREMENT,
+    `url_token` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    `member_id` int(11) NOT NULL,
+    `title` int(11) NOT NULL,
+    `description` varchar(1024),
+    `fields` varchar(1024),
+    `locations` varchar(1024),
+    `target` varchar(1024),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `url_token` (`url_token`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `follow_event`(
+    `member_id` int(11) NOT NULL,
+    `event_id` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    INDEX `member_id` (`member_id` ASC),
+    INDEX `event_id` (`event_id` ASC),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `follow_offer`(
+    `member_id` int(11) NOT NULL,
+    `offer_id` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    INDEX `member_id` (`member_id` ASC),
+    INDEX `offer_id` (`offer_id` ASC),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `member_follow_need`(
+    `member_id` int(11) NOT NULL,
+    `need_id` int(11) NOT NULL,
+    `created` int(11) NOT NULL,
+    INDEX `member_id` (`member_id` ASC),
+    INDEX `need_id` (`need_id` ASC),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
