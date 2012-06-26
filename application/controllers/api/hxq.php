30a183c04f91d9771646c75ceeb883f496261bd7
Bart <jxaa144081@163.com>
2012-06-26 12:52:51 +0800

    modify offer model and controller

--- application/controllers/api/hxq.php
+++ application/controllers/api/hxq.php
@@ -4,35 +4,69 @@ require APPPATH.'/libraries/REST_Controller.php';
 class Hxq extends REST_Controller
 {
 	
-	function offer_get()
+	function offer_get()//$member_id(member_token)='',$time_stamp='now',$num='10',$orderby='created',$offer_id(offer_token)=''
 	{
 		$json = $this->get('json');
 		$arr = json_decode($json,TRUE);
-		$token = $arr['url_token'];
 
-		if(!empty($token))
+		//$this->response($arr,200);
+		$offer_token = $arr['offer_id'];
+
+		$this->load->model('offer_model','',TRUE);
+
+		if(!empty($offer_token))
 		{
-			$this->load->model('offer_model','',TRUE);
-		
-			$offer = $this->offer_model->get_entry_bytoken($token);
-
-			if(!empty($offer))
-			{
-				unset($offer['id']);
-				$this->response($offer,200);
-			}
+			//$offer_token=$this->session->userdata('id');
+			$offer_detail=$this->offer_model->get_entry_bytoken($offer_token);
+			if(!empty($offer_detail))
+				$this->response($offer_detail,200);
 			else
-			{
-				$this->response('token error', 400);
-			}
+				$this->response('no such offer',400);
+		}
+			
+			
+		$number=$arr['num'];
+		$time_stamp=$arr['time_stamp'];
+		$orderby=$arr['orderby'];
+		$member_token=$arr['member_id'];
 
+		if(empty($number))
+			$number=15;
 
+		if(empty($time_stamp))
+		{
+			$time_stamp=time();
+		}
+		
+		if(empty($orderby))
+			$orderby='created';
+
+		
+		if(!empty($member_token))
+		{
+			$this->load->model('member_model','',TRUE);
+			$temp=$this->member_model->get_entry_bytoken($member_token);
+			if(!empty($temp))
+				$member_id=$temp['id'];
+			else
+				$this->response('wrong user id',400);
+		}
+		else
+		{
+			$member_id=$member_token;
+		}
+		
+		$offer = $this->offer_model->get_data_bytoken($member_id,$time_stamp,$number,$orderby);
+
+		if(!empty($offer))
+		{
+			//unset($offer['id']);
+			$this->response($offer,200);
 		}
 		else
 		{
 			$this->response('no token', 400);
-		}
-	
+		}	
 	}
 	
 	function offer_post()
@@ -65,6 +99,11 @@ class Hxq extends REST_Controller
 			$offer_data['member_id']=$arr['member_id'];
 			$offer_data['created']=$arr['created'];
 			$offer_data['url_token']=$arr['url_token'];
+			$offer_data['description']=$arr['description'];
+			$offer_data['fields']=$arr['fields'];
+			$offer_data['locations']=$arr['locations'];
+			$offer_data['target']=$arr['target'];
+
 			$offer = $this->offer_model->insert_entry($offer_data);
 
 			if(!empty($offer))
--- application/models/offer_model.php
+++ application/models/offer_model.php
@@ -9,15 +9,39 @@ class Offer_model extends CI_Model {
 
 	function get_entry_bytoken($token)
 	{
-	echo 'this is get_entry_bytoken';
-	$this->db->select('*');
+	    //echo 'this is get_entry_bytoken';
+	    $this->db->select('*');
         $this->db->from('offer');
         $this->db->where('url_token',$token);
-
+        
         $query = $this->db->get();
 	
         return $query->row_array();
 	}
+
+    function get_data_bytoken($member_id,$time_stamp,$number,$orderby)
+    {
+        $this->db->select('*');
+        $this->db->from('offer');
+        if($member_id!=null)
+            $this->db->where('member_id',$member_id);
+
+        $this->db->where('created <',$time_stamp); // notice there must be a space between 'created' and '<'
+        $this->db->order_by($orderby,'desc');
+        $this->db->limit($number);
+
+        $query = $this->db->get();
+
+        if($query->num_rows()>0)
+        {
+            $result=$query->result_array();
+            return $result;
+        }
+        else
+        {
+            return null;
+        }
+    }
 
 	function insert_entry($offer_data)
     { 
